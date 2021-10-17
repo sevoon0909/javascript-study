174e687eca00c8e1fa147889209cceb5374e0648
sevoon0909 <84161434+sevoon0909@users.noreply.github.com>
2021-10-17 22:34:08 +0900

    #4.5 #4.6 Save Username
    1.localStorage

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
     console.log(username);
     // greeting.innerText = "hello " + username;
-    greeting.innerText = `hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
+function paintGreetings(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `hello ${username}`;
+}
+
+const savedUserName  = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName===null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    paintGreetings(savedUserName);
+}
